8ff203bd3d5564050bf83e90ae874d2a5f5dd6ee
Liang Wang <ryanrhymes@gmail.com>
2019-05-27 00:25:21 +0300

    coding ...

---
+++ app.js
@@ -0,0 +1,26 @@
+const net = require('net')
+
+
+function main() {
+  let sockPath = '/iving/event';
+
+  let client = net.createConnection(sockPath, () => {
+    console.log('connected to iving event hub');
+    client.write('world!\r\n');
+  });
+
+  client.on('data', data => {
+    console.log('======= event arrived =======');
+    console.log(data.toString())
+  });
+
+  client.on('end', () => {
+    console.log('disconnected from iving event hub');
+  });
+
+}
+
+
+// Main function
+
+main()
